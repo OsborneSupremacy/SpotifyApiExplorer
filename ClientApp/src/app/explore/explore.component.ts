33e4110a9cec4768d42ea6af57e55b3fd60d5c33
OsborneSupremacy <osborne.ben@gmail.com>
2019-11-16 09:47:47 -0600

    refactoring

--- ClientApp/src/app/explore/explore.component.ts
+++ ClientApp/src/app/explore/explore.component.ts
@@ -38,91 +38,114 @@ export class ExploreComponent implements OnInit {
         @Inject('SPOTIFY_BASE_URL') private apiBaseUrl: string
     ) {
 
-        this.metricsEnvelope = <MetricEnvelope>{ Populated: false };
+        this.metricsEnvelope = <MetricEnvelope>{
+            Populated: false,
 
-        this.metricsEnvelope.Danceability = <Metric>{
-            Title: `Danceability`,
-            Content: `Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.`,
-            Unit: ``,
-            Min: 0,
-            Max: 1
+            Danceability: <Metric>{
+                Title: `Danceability`,
+                Content: `Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.`,
+                Unit: ``,
+                Min: 0,
+                Max: 1,
+                NominalMin: 0,
+                NominalMax: 1
+            },
+
+            Energy: <Metric>{
+                Title: `Energy`,
+                Content: `Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.`,
+                Unit: ``,
+                Min: 0,
+                Max: 1,
+                NominalMin: 0,
+                NominalMax: 1
+            },
+
+            Loudness: <Metric>{
+                Title: `Loudness`,
+                Content: `The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typical range between -60 and 0 db.`,
+                ImageFile: ``,
+                Unit: `dB`,
+                Min: -49,
+                Max: 2,
+                NominalMin: -60,
+                NominalMax: 0
+            },
+
+            Valence: <Metric>{
+                Title: `Valence`,
+                Content: `A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).`,
+                Unit: ``,
+                Min: 0,
+                Max: 1,
+                NominalMin: 0,
+                NominalMax: 1
+            },
+
+            Speechiness: <Metric>{
+                Title: `Speechiness`,
+                Content: `Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks.`,
+                Unit: ``,
+                Min: 0,
+                Max: 1,
+                NominalMin: 0,
+                NominalMax: 1
+            },
+
+            Acousticness: <Metric>{
+                Title: `Acousticness`,
+                Content: `A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic.`,
+                Unit: ``,
+                Min: 0,
+                Max: 1,
+                NominalMin: 0,
+                NominalMax: 1
+            },
+
+            Instrumentalness: <Metric>{
+                Title: `Instrumentalness`,
+                Content: `Predicts whether a track contains no vocals. “Ooh” and “aah” sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly “vocal”. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0.`,
+                Unit: ``,
+                Min: 0,
+                Max: 1,
+                NominalMin: 0,
+                NominalMax: 1
+            },
+
+            Liveness: <Metric>{
+                Title: `Liveness`,
+                Content: `Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live.`,
+                Unit: ``,
+                Min: 0,
+                Max: 1,
+                NominalMin: 0,
+                NominalMax: 1
+            },
+
+            Tempo: <Metric>{
+                Title: `Tempo`,
+                Content: `The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.`,
+                Unit: `BPM`,
+                Min: 0,
+                Max: 250,
+                NominalMin: 0,
+                NominalMax: 250
+            }
+
         };
 
-        this.metricsEnvelope.Energy = <Metric>{
-            Title: `Energy`,
-            Content: `Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.`,
-            Unit: ``,
-            Min: 0,
-            Max: 1
-        };
+        this.metrics = [
+            this.metricsEnvelope.Danceability,
+            this.metricsEnvelope.Energy,
+            this.metricsEnvelope.Loudness,
+            this.metricsEnvelope.Valence,
+            this.metricsEnvelope.Speechiness,
+            this.metricsEnvelope.Acousticness,
+            this.metricsEnvelope.Instrumentalness,
+            this.metricsEnvelope.Liveness,
+            this.metricsEnvelope.Tempo
+        ]
 
-        this.metricsEnvelope.Loudness = <Metric>{
-            Title: `Loudness`,
-            Content: `The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typical range between -60 and 0 db.`,
-            ImageFile: ``,
-            Unit: `dB`,
-            Min: -60,
-            Max: 0
-        };
-
-        this.metricsEnvelope.Valence = <Metric>{
-            Title: `Valence`,
-            Content: `A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).`,
-            Unit: ``,
-            Min: 0,
-            Max: 1
-        };
-
-        this.metricsEnvelope.Speechiness = <Metric>{
-            Title: `Speechiness`,
-            Content: `Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks.`,
-            Unit: ``,
-            Min: 0,
-            Max: 1
-        };
-
-        this.metricsEnvelope.Acousticness = <Metric>{
-            Title: `Acousticness`,
-            Content: `A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic.`,
-            Unit: ``,
-            Min: 0,
-            Max: 1
-        };
-
-        this.metricsEnvelope.Instrumentalness = <Metric>{
-            Title: `Instrumentalness`,
-            Content: `Predicts whether a track contains no vocals. “Ooh” and “aah” sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly “vocal”. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0.`,
-            Unit: ``,
-            Min: 0,
-            Max: 1
-        };
-
-        this.metricsEnvelope.Liveness = <Metric>{
-            Title: `Liveness`,
-            Content: `Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live.`,
-            Unit: ``,
-            Min: 0,
-            Max: 1
-        };
-
-        this.metricsEnvelope.Tempo = <Metric>{
-            Title: `Tempo`,
-            Content: `The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.`,
-            Unit: `BPM`,
-            Min: 0,
-            Max: 250
-        };
-
-        this.metrics = new Array();
-        this.metrics.push(this.metricsEnvelope.Danceability);
-        this.metrics.push(this.metricsEnvelope.Energy);
-        this.metrics.push(this.metricsEnvelope.Loudness);
-        this.metrics.push(this.metricsEnvelope.Valence);
-        this.metrics.push(this.metricsEnvelope.Speechiness);
-        this.metrics.push(this.metricsEnvelope.Acousticness);
-        this.metrics.push(this.metricsEnvelope.Instrumentalness);
-        this.metrics.push(this.metricsEnvelope.Liveness);
-        this.metrics.push(this.metricsEnvelope.Tempo);
     }
 
     ngOnInit() {
@@ -218,11 +241,11 @@ export class ExploreComponent implements OnInit {
             if (selectedGenre) {
                 selectedGenre.artists.push(artist);
             } else {
-                selectedGenre = new Genre();
-                selectedGenre.name = genreName;
-                selectedGenre.artists = new Array();
-                selectedGenre.tracks = new Array();
-                selectedGenre.artists.push(artist);
+                selectedGenre = <Genre>{
+                    name: genreName,
+                    artists: [ artist ],
+                    tracks: new Array()
+                };
                 this.genres.push(selectedGenre);
             }
             for (let track of artist.tracks)
@@ -252,8 +275,7 @@ export class ExploreComponent implements OnInit {
             if (existingArtist)
                 existingArtist.tracks.push(track);
             if (!existingArtist) {
-                artist.tracks = new Array();
-                artist.tracks.push(track);
+                artist.tracks = [ track ];
                 this.artists.push(artist);
             }
         }
@@ -381,6 +403,8 @@ class Metric {
     Unit: string;
     Min: number;
     Max: number;
+    NominalMin: number;
+    NominalMax: number;
     Value: number;
     MarkerPercentage: number;
 }
